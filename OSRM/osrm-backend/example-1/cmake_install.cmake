f2b3b782b9589d2ef61fa5e784b7c19a480fd665
Arnaud Boissaye <arnaud@T420>
2019-02-04 17:17:48 +0100

    complete make of osrm example next step conversion of the data to use it

--- OSRM/osrm-backend/example-1/cmake_install.cmake
+++ OSRM/osrm-backend/example-1/cmake_install.cmake
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/arnaud/Bureau/PDM/OSRM/Master-Thesis/OSRM/osrm-backend/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/arnaud/Bureau/PDM/OSRM/Master-Thesis/OSRM/osrm-backend/example-1/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

695fdc6d8504e7394aa658cf7fbc764f775ca1de
Arnaud Boissaye <arnaud@T420>
2019-02-05 16:23:10 +0100

    rename du script + update du readme + adaptation de l'architecture global du projet

--- osrm-script.sh
+++ osrm-script.sh
@@ -7,19 +7,18 @@ lua5.2 liblua5.2-dev libtbb-dev
 
 #create and fill up the data folder with the data needed for the example
 # il faudrais faire en sorte que ce soit si ca existe pas déjà
-if [ -e data/monaco/monaco-latest.osm.pbf ]; then
+if [ -e data/OSRM/OSM/monaco/monaco-latest.osm.pbf ]; then
   echo needed data and folder already exist.
 else
   echo data folder creation + fil it up with the monaco data from geofabrik
   mkdir data
-  cd data
-  mkdir monaco
-  cd monaco
-  wget http://download.geofabrik.de/europe/monaco-latest.osm.pbf
-  cd ../..
+  mkdir data/OSRM
+  mkdir data/OSRM/OSM/monaco
+  mkdir data/OSRM/monaco/
+  wget -P ./data/OSRM/OSM/monaco http://download.geofabrik.de/europe/monaco-latest.osm.pbf
 fi
 
-#build the system
+#build the backend
 cd OSRM/osrm-backend
 
 #TODO probably find a way to test if the build has already been done or not.
@@ -39,14 +38,15 @@ make osrm-example
 cd ../../..
 
 #preprocess the data
-./OSRM/osrm-backend/build/osrm-extract data/monaco/monaco-latest.osm.pbf -p \
+./OSRM/osrm-backend/build/osrm-extract data/OSRM/OSM/monaco/monaco-latest.osm.pbf -p \
 OSRM/osrm-backend/profiles/car.lua
-./OSRM/osrm-backend/build/osrm-partition data/monaco/monaco-latest.osrm
-./OSRM/osrm-backend/build/osrm-customize data/monaco/monaco-latest.osrm
+mv data/OSRM/OSM/monaco/monaco-latest.osrm* data/OSRM/monaco/
+./OSRM/osrm-backend/build/osrm-partition data/OSRM/monaco/monaco-latest.osrm
+./OSRM/osrm-backend/build/osrm-customize data/OSRM/monaco/monaco-latest.osrm
 
 #run the example
 echo
 echo example runs and output is : ==============================================
 echo
-./OSRM/osrm-backend/example-1/osrm-example data/monaco/monaco-latest.osrm
+./OSRM/osrm-backend/example-1/osrm-example data/OSRM/monaco/monaco-latest.osrm
 echo

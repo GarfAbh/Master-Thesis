603a181355ed832470b65ebadcb44c1dd53ad7aa
Arnaud Boissaye <arnaud@T420>
2019-02-11 17:12:21 +0100

    make vroom work

---
+++ Vroom/vroom/src/structures/vroom/location.h
@@ -0,0 +1,53 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
+/*
+
+This file is part of VROOM.
+
+Copyright (c) 2015-2018, Julien Coupey.
+All rights reserved (see LICENSE).
+
+*/
+
+#include "structures/typedefs.h"
+
+namespace vroom {
+
+class Location {
+private:
+  // Index of this location in the matrix.
+  Index _index;
+  // Coordinates (not mandatory).
+  OptionalCoordinates _coords;
+  bool _user_index;
+
+public:
+  Location(Index index);
+
+  Location(Index index, const Coordinates& coords);
+
+  Location(const Coordinates& coords);
+
+  void set_index(Index index);
+
+  bool has_coordinates() const;
+
+  Index index() const;
+
+  Coordinate lon() const;
+
+  Coordinate lat() const;
+
+  bool user_index() const;
+
+  // Locations are considered identical if they have the same
+  // user-provided index or if they both have coordinates and those
+  // are equal. The last part is required for situations with no
+  // explicit index provided in input.
+  bool operator==(const Location& other) const;
+};
+
+} // namespace vroom
+
+#endif
